e64df90579d6bfafc259bb3f57ed6dc14800851c
Adib Ahmed <ahmedadib317@gmail.com>
2021-09-14 14:00:17 +0600

    lec 136

--- src/screens/indexScreen.js
+++ src/screens/indexScreen.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useContext } from 'react';
+import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+
+import BlogContext from '../context/BlogContext';
 
 const indexScreen = () => {
+  const { data, addBlogPost } = useContext(BlogContext);
+
   return (
     <View>
       <Text>This is index screen</Text>
+      <Button title='Add Blog' onPress={() => addBlogPost()} />
+      <FlatList
+        data={data}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
